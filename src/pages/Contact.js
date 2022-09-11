d914649ab2addf5ac039d4b9a00e6aecddb2ad05
NatalliaPahosava <mulato4ka114@gmail.com>
2022-09-11 12:09:41 -0400

    styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,11 +3,11 @@ import logo from '../image/2.jpeg'
 const Contact=()=>{
     return(
         <div className="contact"> 
-        <img src={logo} alt="" />
-        <h2>Natalia Pogosova</h2>
-        <a href="https://www.linkedin.com/in/natallia-pahosava/">My Linkedin</a>
-        <a href = "mailto: natalliapahosava@gmail.com">Send Email</a>
-        <a href="tel:123-456-7890">cell phone:456-678-89-78</a>
+            <img src={logo} alt="" />
+            <h2>Natalia Pogosova</h2>
+            <a style={{color:'white'}} href="https://www.linkedin.com/in/natallia-pahosava/">My Linkedin</a>
+            <a href = "mailto: natalliapahosava@gmail.com">Send Email</a>
+            <a href="tel:123-456-7890">cell phone:456-678-89-78</a>
         </div>
     )
 }
